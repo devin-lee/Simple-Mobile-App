ca75a7f823b4ea1aff515e248b190821cc8b1dd4
Devin Lee <40431409+devin-lee@users.noreply.github.com>
2019-11-20 00:34:12 +1100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+View,
+StyleSheet,
+Text,
+} from 'react-native';
+import { Router, Scene } from 'react-native-router-flux';
+
+
+import Menu from './components/menu';
+import Summary from './components/summary';
+import Welcome from './components/welcome';
+
+export default class App extends Component<Props> {
+  //console.disableYellowBox = true;
+  render() {
+    return (
+      <View style={styles.container}>
+        <Router>
+          <Scene key="root">
+            <Scene  initial key="welcome" component = {Welcome} title='Welcome' navigationBarStyle={{ backgroundColor: '#a6192e'}} titleStyle={{color : 'white'}} />
+            <Scene key="menu" component = {Menu} title='Menu' navigationBarStyle={{ backgroundColor: '#a6192e'}} titleStyle={{color : 'white'}}/>
+            <Scene key="summary" component = {Summary} title='Summary' navigationBarStyle={{ backgroundColor: '#a6192e'}} titleStyle={{color : 'white'}}/>
+          </Scene>
+        </Router>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+
+});
